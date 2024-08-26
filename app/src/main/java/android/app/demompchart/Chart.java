018cd90502e3caf8a13f3ab0d16c7548b93f9a55
tranluong <91904056+tranluong24@users.noreply.github.com>
2024-08-27 03:37:07 +0700

    Update Chart.java
    #1 Add LineDataSet, LineData, LineChart
    #2 Demo to check github

--- app/src/main/java/android/app/demompchart/Chart.java
+++ app/src/main/java/android/app/demompchart/Chart.java
@@ -8,9 +8,13 @@ import java.util.Map;
 public class Chart {
 
     ArrayList<Entry> dataValues;
+    LineDataSet lineDataset;
+    LineData lineData;
+    LineChart lineChart;
 
     public Chart(){
         dataValues = new ArrayList<>();
+        lineDataSet = new LineDataSet(dataValues, "Line Chart 1");
     }
 
     public void initChart(){
