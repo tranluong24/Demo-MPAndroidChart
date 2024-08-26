388cb7d7285499efebec6e20201c7a3a26f57434
=tranluong <gohan244123@gmail.com>
2024-08-27 03:48:32 +0700

    Add feat: init, addEntry, updateChart

--- app/src/main/java/android/app/demompchart/Chart.java
+++ app/src/main/java/android/app/demompchart/Chart.java
@@ -1,23 +1,41 @@
 package android.app.demompchart;
 
+import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 
 import java.util.ArrayList;
-import java.util.Map;
 
 public class Chart {
+    private ArrayList<Entry> dataValues;
+    private LineDataSet lineDataset;
+    private LineData lineData;
+    private LineChart lineChart;
 
-    ArrayList<Entry> dataValues;
-    LineDataSet lineDataset;
-    LineData lineData;
-    LineChart lineChart;
-
-    public Chart(){
+    public Chart(LineChart lineChart){
         dataValues = new ArrayList<>();
-        lineDataSet = new LineDataSet(dataValues, "Line Chart 1");
+        lineDataset = new LineDataSet(dataValues, "Line Chart 1");
+        lineData = new LineData(lineDataset);
+        this.lineChart = lineChart;
+        this.lineChart.setData(lineData);
     }
 
     public void initChart(){
-        dataValues.add(new Entry(0,0));
+        lineChart.setAutoScaleMinMaxEnabled(true);
+        lineChart.setVisibleXRangeMaximum(500);
+        lineChart.setVisibleXRangeMinimum(500);
+    }
+
+    public void addEntry(float x, float y){
+        dataValues.add(new Entry(x,y));
+    }
+
+    public void updateChart(){
+        lineDataset.notifyDataSetChanged();
+        lineData.notifyDataChanged();
+        lineChart.notifyDataSetChanged();
+        lineChart.moveViewToX(lineDataset.getEntryCount());
+        lineChart.invalidate();
     }
 }
